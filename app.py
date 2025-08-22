927fbc05c8227770becd1886a9fb9815150545f5
vijaysarikonda <vijay.sarikonda2@gmail.com>
2025-08-22 14:01:18 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask CI/CD pipeline!"
+
+@app.route('/health')
+def health():
+    return {"status": "UP"}
 
 if __name__ == "__main__":
-    print("Hello from Jenkins CI/CD Demo 🚀")
-    print("2 + 3 =", add(2, 3))
+    app.run(host="0.0.0.0", port=5050)
