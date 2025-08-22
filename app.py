ef1f87fa08cb5282b85e1dc818f3bae89d869837
vijaysarikonda <vijay.sarikonda2@gmail.com>
2025-08-22 14:52:08 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask CI/CD pipeline!"
+    return "🚀 Welcome to my First Jenkins pipeline vijay!"
 
 @app.route('/health')
 def health():
@@ -12,7 +12,3 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
